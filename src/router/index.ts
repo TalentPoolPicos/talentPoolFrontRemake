9936d0b5ac5f4fc1d3b905c094c672cab4707d98
avivyh <vivianebarbosa@ufpi.edu.br>
2025-05-22 10:20:02 -0300

    feat: adicionar componentes de NewsView e EnterpriseProfileView

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,40 +1,52 @@
 import { createRouter, createWebHistory } from 'vue-router'
+
 import HomeView from '../views/HomeView.vue'
-<<<<<<< Updated upstream
-import AboutView from "../views/AboutView.vue";
-=======
 import AboutView from '../views/AboutView.vue'
 import NewsView from '../views/NewsView.vue'
->>>>>>> Stashed changes
+
+const LoginView = () => import('../views/LoginView.vue')
+// Make sure the file exists at this path, or update the path if needed
+const StudentProfileView = () => import('../views/StudentProfileView.vue')
+const EnterpriseProfileView = () => import('../views/EnterpriseProfileView.vue')
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView,
+  },
+  {
+    path: '/about',
+    name: 'about',
+    component: AboutView,
+  },
+  {
+    path: '/news',
+    name: 'news',
+    component: NewsView,
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: LoginView,
+  },
+  {
+    path: '/talento/:uuid',
+    name: 'student-profile',
+    component: StudentProfileView,
+    props: true,
+  },
+  {
+    path: '/empresa/:uuid',
+    name: 'enterprise-profile',
+    component: EnterpriseProfileView,
+    props: true,
+  },
+]
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView,
-    },
-    {
-      path: '/about',
-      name: 'about',
-      component: AboutView,
-<<<<<<< Updated upstream
-    }
-=======
-    },
-    {
-      path: '/news',
-      name: 'news',
-      component: NewsView,
-    },
-    {
-      path: '/register',
-      name: 'register',
-      component: () => import('../views/RegisterView.vue'),
-    },
->>>>>>> Stashed changes
-  ],
+  routes,
 })
 
 export default router
