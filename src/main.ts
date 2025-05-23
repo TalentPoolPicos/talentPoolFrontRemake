9411bed9c48a930cfb6ace376a522c31982501d9
Adriano R. de Sousa <adrianorodriguesdsousa@ufpi.edu.br>
2025-05-23 02:07:49 -0300

    style: remove unnecessary semicolon in main.ts for consistency

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
-import { pinia } from './stores';
+import { pinia } from './stores'
 
 import App from './App.vue'
 import router from './router'
