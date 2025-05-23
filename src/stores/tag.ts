00540017a4135e7d6a9cf3c07a9fb7cbd64ca974
Adriano R. de Sousa <adrianorodriguesdsousa@ufpi.edu.br>
2025-05-23 13:11:28 -0300

    feat: add tag store with CRUD operations and user integration

---
+++ src/stores/tag.ts
@@ -0,0 +1,33 @@
+import { defineStore } from 'pinia'
+import { useUserStore } from './user'
+import type { components } from '@/types/api'
+import { http } from '@/services/http'
+
+type TagDto = components['schemas']['TagDto']
+type CreateTagDto = components['schemas']['CreateTagDto']
+
+export const useTagStore = defineStore('tag', () => {
+  const userStore = useUserStore()
+
+  const findByUuid = async (uuid: string): Promise<TagDto> => {
+    const { data } = await http.get<TagDto>(`/tag/${uuid}`)
+    return data
+  }
+
+  const create = async (tag: CreateTagDto): Promise<TagDto> => {
+    const { data } = await http.post<TagDto>(`/tag`, tag)
+    userStore.fetch()
+    return data
+  }
+
+  const remove = async (uuid: string): Promise<void> => {
+    await http.delete(`/tag/${uuid}`)
+    userStore.fetch()
+  }
+
+  return {
+    findByUuid,
+    create,
+    remove,
+  }
+})
