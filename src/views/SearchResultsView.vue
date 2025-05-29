9864e54b3d19d23335bf78fcac8f078529a20eef
Vandirleya B. <vandirleya.barbosa@ufpi.edu.br>
2025-05-28 22:47:57 -0300

    feat: implement fetchResults function for handling search queries

--- src/views/SearchResultsView.vue
+++ src/views/SearchResultsView.vue
@@ -11,3 +11,25 @@ const route = useRoute()
 const query = ref(route.query.q as string || '')
 const results = ref<SearchResultDto>()
 const loading = ref(false)
+
+const fetchResults = async (q: string) => {
+  if (!q) return
+
+  loading.value = true
+
+  try {
+    const response = await http.get<SearchResultDto>(`/search/${q}`, {})
+    const data = response.data
+    results.value = data
+
+    console.log('Quantidade', data.users.length)
+  } catch (err) {
+    console.error('Erro na busca:', err)
+    results.value = {
+      users: [],
+      total: 0,
+    }
+  } finally {
+    loading.value = false
+  }
+}
